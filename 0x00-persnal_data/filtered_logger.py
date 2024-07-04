bbf1149ced4780b8a640731b115883e560917083
gh057 <victormurithi2020@gmail.com>
2024-07-05 01:41:10 +0300

    without re

---
+++ 0x00-persnal_data/filtered_logger.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Mock function of a logger builtin function"""
+from typing import List
+import re
+
+
+def filter_datum(fields: List[str], redaction: str, message: str, seperator: str) -> str:
+    """function that tries to replicate the logger function"""
+    data = message.split(';')
+    for idx, item in enumerate(data):
+        if item.split('=')[0] in fields:
+            data[idx] = item.replace(item.split('=')[1], redaction)
+    return f"{seperator}".join(data)
