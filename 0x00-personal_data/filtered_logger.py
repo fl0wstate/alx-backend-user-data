db718461b4ff3a6dfc0e5c361ee5aab6dbfb05e1
gh057 <victormurithi2020@gmail.com>
2024-07-05 05:10:54 +0300

    fix task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -19,12 +19,10 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
-        """init"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """log formatter"""
         original_message = super(RedactingFormatter, self).format(record)
         redacted_message = filter_datum(self.fields, self.REDACTION,
                                         original_message, self.SEPARATOR)
