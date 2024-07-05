3efe81d842f6bdd60dbf8aa4d2688e360c2c80d4
gh057 <victormurithi2020@gmail.com>
2024-07-05 04:03:04 +0300

    fix

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,11 +8,9 @@ def filter_datum(fields: List[str],
                  redaction: str,
                  message: str, separator: str) -> str:
   """Redacts specified fields in a message using regex."""
-    pattern = r'(\w+)=([^;]+)'
-    return re.sub(pattern,
-                  lambda match:
-                  f"{match.group(1)}={redaction}{separator}"
-                  if match.group(1) in fields
-                  else f"{match.group(0)}{separator}",
-                  message
-                  )
+  return re.sub(r'(\w+)=([^;]+)',
+                lambda match:
+                f"{match.group(1)}={redaction}{separator}"
+                if match.group(1) in fields
+                else f"{match.group(0)}{separator}",
+                message)
