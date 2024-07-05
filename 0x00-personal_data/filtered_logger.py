534349be13032eee812fcac88d15566ca001f90b
gh057 <victormurithi2020@gmail.com>
2024-07-05 05:16:03 +0300

    task 3

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -20,11 +20,31 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
+        """init"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """formatter"""
         original_message = super(RedactingFormatter, self).format(record)
         redacted_message = filter_datum(self.fields, self.REDACTION,
                                         original_message, self.SEPARATOR)
         return redacted_message
+
+
+def get_logger() -> logging.Logger:
+    """Creates and returns a logger for user data."""
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    # Create a StreamHandler
+    stream_handler = logging.StreamHandler()
+    # Create a RedactingFormatter and set it for the handler
+    formatter = RedactingFormatter(list(PII_FIELDS))
+    stream_handler.setFormatter(formatter)
+
+    # Add the handler to the logger
+    logger.addHandler(stream_handler)
+
+    return logger
