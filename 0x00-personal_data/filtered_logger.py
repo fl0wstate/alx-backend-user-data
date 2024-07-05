6615a19adcf810cd2f607b3fdd9207812fbf5b0a
gh057 <victormurithi2020@gmail.com>
2024-07-05 04:57:58 +0300

    fix4

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,14 +4,8 @@ from typing import List
 import re
 
 
-def filter_datum(fields: List[str],
-                 redaction: str,
+def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
-    """Redacts specified fields in a message using regex."""
-    return re.sub(r'(\w+)=([^;]+)',
-                  lambda match:
-                  f"{match.group(1)}={redaction}{separator}"
-                  if match.group(1) in fields
-                  else f"{match.group(0).replace(
-                    match.group(0)[-1], separator
-                  )}", message)
+    '''returns the log message obfuscated'''
+    pattern = '|'.join(f"(?<={field}=)[^{separator}]+" for field in fields)
+    return re.sub(pattern, redaction, message)
