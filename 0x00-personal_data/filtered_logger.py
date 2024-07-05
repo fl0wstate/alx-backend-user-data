7c3c4faa15f0eac811331d32c1f8b3ba6cf42cf2
gh057 <victormurithi2020@gmail.com>
2024-07-05 05:08:36 +0300

    fix task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -9,3 +9,23 @@ def filter_datum(fields: List[str], redaction: str,
     '''returns the log message obfuscated'''
     pattern = '|'.join(f"(?<={field}=)[^{separator}]+" for field in fields)
     return re.sub(pattern, redaction, message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """init"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """log formatter"""
+        original_message = super(RedactingFormatter, self).format(record)
+        redacted_message = filter_datum(self.fields, self.REDACTION,
+                                        original_message, self.SEPARATOR)
+        return redacted_message
