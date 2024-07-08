835c80990a3ba47155b49b59070bc178a8337335
gh057 <victormurithi2020@gmail.com>
2024-07-08 10:21:33 +0300

    updated the database env name

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -62,7 +62,7 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
             user=os.getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
             password=os.getenv('PERSONAL_DATA_DB_PASSWORD', ''),
             host=os.getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
-            database=os.getenv('PERSONAL_DATA_NAME')
+            database=os.getenv('PERSONAL_DATA_DB_NAME')
     )
 
 
