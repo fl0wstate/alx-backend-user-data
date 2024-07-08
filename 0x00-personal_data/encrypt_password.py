6b80496623f35d1767aa5096dcb66bf31ca3b8e7
gh057 <victormurithi2020@gmail.com>
2024-07-08 10:44:54 +0300

    updated an valid encrypted checker

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -9,3 +9,8 @@ def hash_password(password: str) -> bytes:
     bits = password.encode()
     new_pwd = bcrypt.hashpw(bits, _salt)
     return new_pwd
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """validates the hashed passwords"""
+    return (bcrypt.checkpw(password.encode(), hashed_password))
