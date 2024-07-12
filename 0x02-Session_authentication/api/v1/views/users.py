eb4dc1358739519b962d58c8c2a6bc4700d611ad
gh057 <victormurithi2020@gmail.com>
2024-07-12 04:33:20 +0300

    errors

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -26,7 +26,7 @@ def view_one_user(user_id: str = None) -> str:
       - 404 if the User ID doesn't exist
     """
     # Task 0
-    if user_id = "me":
+    if user_id == "me":
         if request.create_user is None:
             abort(404)
         return jsonify(request.create_user.to_json())
