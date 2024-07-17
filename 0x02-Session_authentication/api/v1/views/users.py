40497e540c30d7dae78bc35886d33bc7b2604a94
gh057 <victormurithi2020@gmail.com>
2024-07-18 02:54:30 +0300

    fixed the wrong request key value pairs

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -29,7 +29,7 @@ def view_one_user(user_id: str = None) -> str:
         abort(404)
 
     if user_id == "me":
-        if request.create_user is None:
+        if request.current_user is None:
             abort(404)
         return jsonify(request.current_user.to_json())
 
