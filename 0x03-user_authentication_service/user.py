25cf868806b712ce32e018bbae89250ca704a21a
mk <victormurith2020@gmail.com>
2024-07-19 00:26:41 +0300

    Added the users table using python class through sqlalchemy

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Simple User Database"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    """User class which will become the table in sql"""
+    __tablename__ = 'users'
+
+    id = Column('id', Integer, primary_key=True)
+    email = Column('email', String(250), nullable=False)
+    hashed_password = Column('hashed_password', String(250), nullable=False)
+    session_id = Column('session_id', String(250), nullable=True)
+    reset_token = Column('reset_token', String(250), nullable=True)
