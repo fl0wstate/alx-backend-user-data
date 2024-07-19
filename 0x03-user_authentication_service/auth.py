d9c2d7390d6a2698f751c9d3c804d97400ce78ec
mk <victormurith2020@gmail.com>
2024-07-19 03:58:18 +0300

    hashing user passwd

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -21,14 +21,13 @@ class Auth:
     def __init__(self):
         self._db = DB()
 
-
     def register_user(self, email: str, password: str) -> User:
         """returns a user whose password has been hashed when
         registered to the datbase"""
         # find the user by email
         try:
             user = self._db.find_user_by(email=email)
-            raise ValueError(f"User {user.email} already exists")
+            raise ValueError(f"User {email} already exists")
         except NoResultFound:
             pass
         hashed_pass = _hash_password(password)
