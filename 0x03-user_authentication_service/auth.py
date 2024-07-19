d0160e03d3cb781042e53f928fbb444fdeb1041a
mk <victormurith2020@gmail.com>
2024-07-19 03:52:08 +0300

    hashing user passwd

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,6 +2,9 @@
 """Hashing password method"""
 
 import bcrypt
+from sqlalchemy.exc import NoResultFound
+from db import DB
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
@@ -9,3 +12,24 @@ def _hash_password(password: str) -> bytes:
     return bcrypt.hashpw(
             password.encode('utf-8'),
             bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+
+    def register_user(self, email: str, password: str) -> User:
+        """returns a user whose password has been hashed when
+        registered to the datbase"""
+        # find the user by email
+        try:
+            user = self._db.find_user_by(email=email)
+            raise ValueError(f"User {user.email} already exists")
+        except NoResultFound:
+            pass
+        hashed_pass = _hash_password(password)
+        return self._db.add_user(email, hashed_pass.decode('utf-8'))
