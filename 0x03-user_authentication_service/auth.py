804c9f81f167885ab1aa026ed766ac9f20d36bc1
mk <victormurith2020@gmail.com>
2024-07-22 11:34:33 +0300

    reset_token should be set to None after password update

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -104,5 +104,6 @@ class Auth:
             user = self._db.find_user_by(reset_token=reset_token)
             hashed_pwd = _hash_password(password)
             self._db.update_user(user.id, hashed_password=hashed_pwd)
+            self._db.update_user(user.id, reset_token=None)
         except Exception:
             raise ValueError
