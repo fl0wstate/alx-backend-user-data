ceb712e590478b43e2e6b4f657b77ae17e5ebc0e
mk <victormurith2020@gmail.com>
2024-07-22 08:20:09 +0300

    updated'

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -70,7 +70,7 @@ class Auth:
             return None
 
     def get_user_from_session_id(
-            self, session_id: str) -> Optional[User | None]:
+            self, session_id: str) -> User:
         """Returns a user based on the session_id given"""
         if session_id is None:
             return None
