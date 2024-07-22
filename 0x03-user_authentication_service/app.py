c90c656ce213b7e6eb2b0bd8f4e1f8ceb9d0263c
mk <victormurith2020@gmail.com>
2024-07-22 11:59:29 +0300

    updated the function for updateing pwasword

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -115,5 +115,26 @@ def get_reset_password_token():
             abort(403)
 
 
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+def update_password():
+    """endpoint for handling password reset for all the users
+    currently requesting password reset"""
+    email = request.form.get('email')
+    reset_token = request.form.get('reset_token')
+    new_password = request.form.get('new_password')
+
+    if not email or not reset_token or not new_password:
+        abort(403)
+
+    try:
+        AUTH.update_password(reset_token, new_password)
+        return jsonify({
+            "email": email,
+            "message": "Password updated"
+        })
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
